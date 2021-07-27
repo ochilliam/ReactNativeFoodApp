b012d7742cef3a0e630c472eae6c32237279a56a
ochilliam <ochilliam@gmail.com>
2021-07-27 17:54:35 +0300

    fix(): duplicate ids on data. add uuid to deps.

--- constants/dummyData.ts
+++ constants/dummyData.ts
@@ -1,271 +1,265 @@
-import images from "./images"
-import icons from "./icons"
+import "react-native-get-random-values";
+import { v4 as uuid } from "uuid";
+
+import images from "./images";
+import icons from "./icons";
 
 const trendingRecipes = [
-    {
-        id: 1,
-        name: "Spaghetti With Shrimp Sauce",
-        image: images.spagetti,
-        duration: "30 mins",
-        serving: 1,
-        isBookmark: false,
-        category: "Pasta",
-        author: {
-            profilePic: images.UserProfile5,
-            name: "Maria",
-        },
-        ingredients: [
-            {
-                id: 1,
-                icon: icons.pasta,
-                description: "Spaghetti pasta",
-                quantity: "100g"
-            },
-            {
-                id: 2,
-                icon: icons.oil,
-                description: "Olive Oil",
-                quantity: "2 tbsp"
-            },
-            {
-                id: 3,
-                icon: icons.shrimp,
-                description: "Fresh Shrimp",
-                quantity: "100g"
-            },
-            {
-                id: 4,
-                icon: icons.tomato,
-                description: "Campari tomatoes",
-                quantity: "100g"
-            },
-            {
-                id: 5,
-                icon: icons.salt,
-                description: "Salt",
-                quantity: "¾ tbsp"
-            },
-            {
-                id: 6,
-                icon: icons.pepper,
-                description: "Black Pepper",
-                quantity: "¼ tbsp"
-            },
-
-        ],
-        viewers: [
-            {
-                id: 1,
-                profilePic: images.UserProfile1
-            },
-            {
-                id: 2,
-                profilePic: images.UserProfile2
-            },
-            {
-                id: 3,
-                profilePic: images.UserProfile3
-            },
-            {
-                id: 4,
-                profilePic: images.UserProfile3
-            }
-        ]
+  {
+    id: uuid(),
+    name: "Spaghetti With Shrimp Sauce",
+    image: images.spagetti,
+    duration: "30 mins",
+    serving: 1,
+    isBookmark: false,
+    category: "Pasta",
+    author: {
+      profilePic: images.UserProfile5,
+      name: "Maria",
     },
-    {
-        id: 2,
-        name: "Malaysian Chicken Satay",
-        image: images.satay,
-        duration: "50 mins",
-        serving: 10,
-        isBookmark: true,
-        category: "Local",
-        author: {
-            profilePic: images.UserProfile8,
-            name: "Mandy",
-        },
-        ingredients: [
-            {
-                id: 1,
-                icon: icons.chicken,
-                description: "Boneless Chicken Thighs",
-                quantity: "1kg"
-            },
-            {
-                id: 2,
-                icon: icons.lemongrass,
-                description: "Lemongrass stalk",
-                quantity: "1 stalk"
-            },
-            {
-                id: 3,
-                icon: icons.onion,
-                description: "Large Onion",
-                quantity: "1"
-            },
-            {
-                id: 4,
-                icon: icons.garlic,
-                description: "Garlic cloves",
-                quantity: "5"
-            },
-            {
-                id: 5,
-                icon: icons.coriander,
-                description: "Coriander",
-                quantity: "1 tsp"
-            },
-
-        ],
-        viewers: [
-            {
-                id: 1,
-                profilePic: images.UserProfile5
-            },
-            {
-                id: 2,
-                profilePic: images.UserProfile4
-            },
-            {
-                id: 3,
-                profilePic: images.UserProfile1
-            },
-            {
-                id: 4,
-                profilePic: images.UserProfile2
-            },
-            {
-                id: 5,
-                profilePic: images.UserProfile3
-            }
-        ]
+    ingredients: [
+      {
+        id: uuid(),
+        icon: icons.pasta,
+        description: "Spaghetti pasta",
+        quantity: "100g",
+      },
+      {
+        id: uuid(),
+        icon: icons.oil,
+        description: "Olive Oil",
+        quantity: "2 tbsp",
+      },
+      {
+        id: uuid(),
+        icon: icons.shrimp,
+        description: "Fresh Shrimp",
+        quantity: "100g",
+      },
+      {
+        id: uuid(),
+        icon: icons.tomato,
+        description: "Campari tomatoes",
+        quantity: "100g",
+      },
+      {
+        id: uuid(),
+        icon: icons.salt,
+        description: "Salt",
+        quantity: "¾ tbsp",
+      },
+      {
+        id: uuid(),
+        icon: icons.pepper,
+        description: "Black Pepper",
+        quantity: "¼ tbsp",
+      },
+    ],
+    viewers: [
+      {
+        id: uuid(),
+        profilePic: images.UserProfile1,
+      },
+      {
+        id: uuid(),
+        profilePic: images.UserProfile2,
+      },
+      {
+        id: uuid(),
+        profilePic: images.UserProfile3,
+      },
+      {
+        id: uuid(),
+        profilePic: images.UserProfile3,
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    name: "Malaysian Chicken Satay",
+    image: images.satay,
+    duration: "50 mins",
+    serving: 10,
+    isBookmark: true,
+    category: "Local",
+    author: {
+      profilePic: images.UserProfile8,
+      name: "Mandy",
     },
-    {
-        id: 3,
-        name: "Sarawak Laksa",
-        image: images.laksa,
-        duration: "30 mins",
-        serving: 1,
-        isBookmark: true,
-        category: "Local",
-        author: {
-            profilePic: images.UserProfile9,
-            name: "Jessie",
-        },
-        ingredients: [
-            {
-                id: 1,
-                icon: icons.garlic,
-                description: "Garlic cloves",
-                quantity: "3"
-            },
-            {
-                id: 2,
-                icon: icons.lemongrass,
-                description: "Lemongrass",
-                quantity: "2 stalks"
-            },
-            {
-                id: 3,
-                icon: icons.egg,
-                description: "Egg",
-                quantity: "2"
-            },
-            {
-                id: 4,
-                icon: icons.shrimp,
-                description: "Fresh Shrimp",
-                quantity: "100g"
-            },
-            {
-                id: 5,
-                icon: icons.shallot,
-                description: "Shallot",
-                quantity: "4"
-            },
-            {
-                id: 6,
-                icon: icons.pasta,
-                description: "vermicelli",
-                quantity: "100g"
-            },
-
-
-        ],
-        viewers: [
-            {
-                id: 1,
-                name: "User 1",
-                profilePic: images.UserProfile1
-            },
-            {
-                id: 2,
-                name: "User 2",
-                profilePic: images.UserProfile2
-            },
-            {
-                id: 3,
-                name: "User 3",
-                profilePic: images.UserProfile3
-            }
-        ]
+    ingredients: [
+      {
+        id: uuid(),
+        icon: icons.chicken,
+        description: "Boneless Chicken Thighs",
+        quantity: "1kg",
+      },
+      {
+        id: uuid(),
+        icon: icons.lemongrass,
+        description: "Lemongrass stalk",
+        quantity: "1 stalk",
+      },
+      {
+        id: uuid(),
+        icon: icons.onion,
+        description: "Large Onion",
+        quantity: "1",
+      },
+      {
+        id: uuid(),
+        icon: icons.garlic,
+        description: "Garlic cloves",
+        quantity: "5",
+      },
+      {
+        id: uuid(),
+        icon: icons.coriander,
+        description: "Coriander",
+        quantity: "1 tsp",
+      },
+    ],
+    viewers: [
+      {
+        id: uuid(),
+        profilePic: images.UserProfile5,
+      },
+      {
+        id: uuid(),
+        profilePic: images.UserProfile4,
+      },
+      {
+        id: uuid(),
+        profilePic: images.UserProfile1,
+      },
+      {
+        id: uuid(),
+        profilePic: images.UserProfile2,
+      },
+      {
+        id: uuid(),
+        profilePic: images.UserProfile3,
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    name: "Sarawak Laksa",
+    image: images.laksa,
+    duration: "30 mins",
+    serving: 1,
+    isBookmark: true,
+    category: "Local",
+    author: {
+      profilePic: images.UserProfile9,
+      name: "Jessie",
     },
-    {
-        id: 4,
-        name: "Nasi Lemak",
-        image: images.nasiLemak,
-        duration: "1 hour",
-        serving: 10,
-        isBookmark: true,
-        category: "Local",
-        author: {
-            profilePic: images.UserProfile7,
-            name: "Ali Baba",
-        },
-        ingredients: [
-            {
-                id: 1,
-                icon: icons.chilli,
-                description: "Dried Chilli",
-                quantity: "30g"
-            },
-            {
-                id: 2,
-                icon: icons.garlic,
-                description: "Garlic cloves",
-                quantity: "3"
-            },
-            {
-                id: 3,
-                icon: icons.egg,
-                description: "Egg",
-                quantity: "10"
-            },
-            {
-                id: 4,
-                icon: icons.rice,
-                description: "rice",
-                quantity: "1kg"
-            },
-            {
-                id: 5,
-                icon: icons.anchovy,
-                description: "Dried anchovies",
-                quantity: "3 cups"
-            },
-
-
-        ],
-        viewers: [
-
-        ]
+    ingredients: [
+      {
+        id: uuid(),
+        icon: icons.garlic,
+        description: "Garlic cloves",
+        quantity: "3",
+      },
+      {
+        id: uuid(),
+        icon: icons.lemongrass,
+        description: "Lemongrass",
+        quantity: "2 stalks",
+      },
+      {
+        id: uuid(),
+        icon: icons.egg,
+        description: "Egg",
+        quantity: "2",
+      },
+      {
+        id: uuid(),
+        icon: icons.shrimp,
+        description: "Fresh Shrimp",
+        quantity: "100g",
+      },
+      {
+        id: uuid(),
+        icon: icons.shallot,
+        description: "Shallot",
+        quantity: "4",
+      },
+      {
+        id: uuid(),
+        icon: icons.pasta,
+        description: "vermicelli",
+        quantity: "100g",
+      },
+    ],
+    viewers: [
+      {
+        id: uuid(),
+        name: "User 1",
+        profilePic: images.UserProfile1,
+      },
+      {
+        id: uuid(),
+        name: "User 2",
+        profilePic: images.UserProfile2,
+      },
+      {
+        id: uuid(),
+        name: "User 3",
+        profilePic: images.UserProfile3,
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    name: "Nasi Lemak",
+    image: images.nasiLemak,
+    duration: "1 hour",
+    serving: 10,
+    isBookmark: true,
+    category: "Local",
+    author: {
+      profilePic: images.UserProfile7,
+      name: "Ali Baba",
     },
+    ingredients: [
+      {
+        id: uuid(),
+        icon: icons.chilli,
+        description: "Dried Chilli",
+        quantity: "30g",
+      },
+      {
+        id: uuid(),
+        icon: icons.garlic,
+        description: "Garlic cloves",
+        quantity: "3",
+      },
+      {
+        id: uuid(),
+        icon: icons.egg,
+        description: "Egg",
+        quantity: "10",
+      },
+      {
+        id: uuid(),
+        icon: icons.rice,
+        description: "rice",
+        quantity: "1kg",
+      },
+      {
+        id: uuid(),
+        icon: icons.anchovy,
+        description: "Dried anchovies",
+        quantity: "3 cups",
+      },
+    ],
+    viewers: [],
+  },
+];
 
-]
-
-const categories = trendingRecipes
+const categories = trendingRecipes;
 
 export default {
-    trendingRecipes,
-    categories
-}
+  trendingRecipes,
+  categories,
+};
